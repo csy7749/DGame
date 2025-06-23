8a25eba5b8a658e876f1ffaae5f4719662aeec48
Rock <799375868@qq.com>
2025-06-24 00:43:43 +0800

    Init Project

---
+++ GameUnity/Assets/DGame/Editor/DefineSymbols/DGameLogScriptingDefineSymbols.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DGame.Editor
+{
+    public static partial class ScriptingDefineSymbolsTools
+    {
+        private const string ENABLE_LOG_SCRIPTING_DEFINE_SYMBOL = "DGAME_LOG";
+    }
+}
