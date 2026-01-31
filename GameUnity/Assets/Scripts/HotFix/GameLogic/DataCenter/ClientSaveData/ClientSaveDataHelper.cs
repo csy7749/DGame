d4e0f2330c922f1cf3be071ee36759059c0fc439
Rock <799375868@qq.com>
2026-01-31 17:33:29 +0800

    增加拓展方法

---
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/DataCenter/ClientSaveData/ClientSaveDataHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace GameLogic
+{
+    public static class ClientSaveDataHelper
+    {
+        public static int GetSystemSettingVal(this SystemSaveData saveData, SystemSaveData.SaveType saveType)
+            => saveData.SettingParams[(int)saveType];
+
+        public static void SaveSystemSettingVal(this SystemSaveData saveData, SystemSaveData.SaveType saveType,
+            int value)
+        {
+            saveData?.SetSystemSettingVal(saveType, value);
+            saveData?.Save();
+        }
+
+        public static void SetSystemSettingVal(this SystemSaveData saveData, SystemSaveData.SaveType saveType, int value)
+        {
+            saveData.SettingParams[(int)saveType] = value;
+        }
+    }
+}
