eb68e50fb41dbab7132c085eccdd9f9d36e18111
Jun <799375868@qq.com>
2026-02-04 18:01:19 +0800

    FrameSpritePool自动生成器

---
+++ Tools/Generata Tools/SourceGenerator/FrameSpritePoolGenerator/Definition.cs
@@ -0,0 +1,9 @@
+﻿namespace FrameSpritePoolGenerator;
+
+public class Definition
+{
+    public const string NameSpace = "GameLogic";//"DGame";
+    public static readonly string[] UsingNameSpace = ["UnityEngine", "System", "System.Collections.Generic"];
+    public static readonly List<string> TargetNameSpaces = ["GameLogic"];
+    public const string EnumName = "FrameAnimName";
+}
