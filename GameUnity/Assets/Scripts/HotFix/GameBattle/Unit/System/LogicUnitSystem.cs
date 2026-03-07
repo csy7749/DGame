ebc753bdf65c3b0ca77dfa3a338186932b6fbefa
Rock <799375868@qq.com>
2026-03-07 20:09:27 +0800

    Unit的System

---
+++ GameUnity/Assets/Scripts/HotFix/GameBattle/Unit/System/LogicUnitSystem.cs
@@ -0,0 +1,17 @@
+﻿using Fantasy.Entitas.Interface;
+
+namespace GameBattle
+{
+    public sealed class LogicUnitDestroySystem : DestroySystem<LogicUnit>
+    {
+        protected override void Destroy(LogicUnit self)
+        {
+            self.RenderUnit = null;
+        }
+    }
+    
+    public sealed class LogicUnitSystem
+    {
+        
+    }
+}
---
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/BattleSys/Unit/System/RenderUnitSystem.cs
@@ -0,0 +1,20 @@
+﻿using Fantasy.Entitas.Interface;
+
+namespace GameLogic
+{
+    public sealed class RenderUnitDestroySystem : DestroySystem<RenderUnit>
+    {
+        protected override void Destroy(RenderUnit self)
+        {
+            if (self.IsDestroyed)
+            {
+                return;
+            }
+        }
+    }
+    
+    public sealed class RenderUnitSystem
+    {
+        
+    }
+}
