874a9aa2c2cb197ed2a3d8bc832f5bcff7bb9da4
Jun <799375868@qq.com>
2026-03-09 11:33:32 +0800

    copy luban的bat文件

---
+++ Tools/LubanTools/path_define.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SOURCE="$SCRIPT_DIR/Luban"
+DEST="$SCRIPT_DIR/../../GameConfig/Tools/LubanTools/Luban"
+DEST_PARENT="$SCRIPT_DIR/../../GameConfig/Tools/LubanTools"
