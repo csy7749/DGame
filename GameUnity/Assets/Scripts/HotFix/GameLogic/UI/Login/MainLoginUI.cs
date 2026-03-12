0c08de6e98b8a2bd45e605bd92a53904b0eec214
Jun <799375868@qq.com>
2026-03-12 13:39:44 +0800

    MainLoginUI

---
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/UI/Login/MainLoginUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DGame;
+
+namespace GameLogic
+{
+	public partial class MainLoginUI
+	{
+		#region 事件
+
+		private partial void OnClickAgeTipsBtn()
+		{
+		}
+
+		private partial void OnToggleUserPrivacyChange(bool isOn)
+		{
+		}
+
+		private partial void OnClickUserAgreementBtn()
+		{
+		}
+
+		private partial void OnClickUserPrivacyBtn()
+		{
+		}
+
+		private partial void OnClickStartGameBtn()
+		{
+		}
+
+		private partial void OnClickSelectServerBtn()
+		{
+		}
+
+		private partial void OnClickUserBtn()
+		{
+		}
+
+		private partial void OnClickNotifyBtn()
+		{
+		}
+
+		private partial void OnClickCloseAccountBtn()
+		{
+		}
+
+		private partial void OnClickLoginBtn()
+		{
+		}
+
+		#endregion
+	}
+}
