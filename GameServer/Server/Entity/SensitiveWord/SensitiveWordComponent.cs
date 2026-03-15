a2d46ca97d449a45b25ef95f94c997d85f7b593d
Amani <799375868@qq.com>
2026-03-15 19:21:12 +0800

    添加服务器敏感词检测

---
+++ GameServer/Server/Entity/SensitiveWord/SensitiveWordComponent.cs
@@ -0,0 +1,128 @@
+﻿using Fantasy.Entitas;
+using ToolGood.Words;
+
+#pragma warning disable CS0618 // 类型或成员已过时
+
+namespace Fantasy;
+
+public class SensitiveWordComponent : Entity
+{
+    public IllegalWordsSearch IllegalWordsSearch { get; set; }
+    public WordsSearch WordsSearch { get; set; }
+    private bool m_useAdvancedFilter = true;
+    private bool m_useDBCcaseConverter = true;
+    private bool m_useIgnoreCase = true;
+    private bool m_useDuplicateWordFilter = true;
+    private bool m_useSkipWordFilter = true;
+
+    /// <summary>
+    /// 是否使用高级过滤
+    /// </summary>
+    public bool UseAdvancedFilter
+    {
+        get => m_useAdvancedFilter;
+        set
+        {
+            if (m_useAdvancedFilter != value)
+            {
+                m_useAdvancedFilter = value;
+
+                if (m_useAdvancedFilter)
+                {
+                    IllegalWordsSearch = new IllegalWordsSearch();
+                    WordsSearch = null;
+                }
+                else
+                {
+                    IllegalWordsSearch = null;
+                    WordsSearch = new WordsSearch();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 设置是否全角/半角自动转换
+    /// </summary>
+    public bool UseDBCcaseConverter
+    {
+        get => m_useDBCcaseConverter;
+        set
+        {
+            if (m_useDBCcaseConverter != value)
+            {
+                m_useDBCcaseConverter = value;
+
+                if (IllegalWordsSearch != null)
+                {
+                    IllegalWordsSearch.UseDBCcaseConverter = m_useDBCcaseConverter;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 设置是否忽略大小写
+    /// </summary>
+    public bool UseIgnoreCase
+    {
+        get => m_useIgnoreCase;
+        set
+        {
+            if (m_useIgnoreCase != value)
+            {
+                m_useIgnoreCase = value;
+
+                if (IllegalWordsSearch != null)
+                {
+                    IllegalWordsSearch.UseIgnoreCase = m_useIgnoreCase;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 设置是否自动识别重复字符
+    /// </summary>
+    public bool UseDuplicateWordFilter
+    {
+        get => m_useDuplicateWordFilter;
+        set
+        {
+            if (m_useDuplicateWordFilter != value)
+            {
+                m_useDuplicateWordFilter = value;
+
+                if (IllegalWordsSearch != null)
+                {
+                    IllegalWordsSearch.UseDuplicateWordFilter = m_useDuplicateWordFilter;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 设置是否开启跳词检测
+    /// </summary>
+    public bool UseSkipWordFilter
+    {
+        get => m_useSkipWordFilter;
+        set
+        {
+            if (m_useSkipWordFilter != value)
+            {
+                m_useSkipWordFilter = value;
+
+                if (IllegalWordsSearch != null)
+                {
+                    IllegalWordsSearch.UseSkipWordFilter = m_useSkipWordFilter;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 敏感词替换字符
+    /// </summary>
+    public char ReplaceChar { get; set; } = '*';
+}
---
+++ GameServer/Server/Hotfix/SensitiveWord/System/SensitiveWordComponentSystem.cs
@@ -0,0 +1,253 @@
+﻿using Fantasy;
+using Fantasy.Entitas.Interface;
+using ToolGood.Words;
+#pragma warning disable CS0618 // 类型或成员已过时
+
+namespace Hotfix;
+
+public sealed class SensitiveWordComponentAwakeSystem : AwakeSystem<SensitiveWordComponent>
+{
+    protected override void Awake(SensitiveWordComponent self)
+    {
+        if (self.UseAdvancedFilter)
+        {
+            self.IllegalWordsSearch = new IllegalWordsSearch();
+            self.IllegalWordsSearch.UseIgnoreCase = self.UseIgnoreCase;
+            self.IllegalWordsSearch.UseDBCcaseConverter = self.UseDBCcaseConverter;
+            self.IllegalWordsSearch.UseDuplicateWordFilter = self.UseDuplicateWordFilter;
+        }
+        else
+        {
+            self.WordsSearch = new WordsSearch();
+        }
+    }
+}
+
+public sealed class SensitiveWordComponentDestroySystem : DestroySystem<SensitiveWordComponent>
+{
+    protected override void Destroy(SensitiveWordComponent self)
+    {
+        self.IllegalWordsSearch = null;
+        self.WordsSearch = null;
+    }
+}
+
+public static class SensitiveWordComponentSystem
+{
+    /// <summary>
+    /// 设置敏感词库
+    /// </summary>
+    /// <param name="keywords">敏感词库</param>
+    public static void SetKeywords(this SensitiveWordComponent self, List<string> keywords)
+    {
+        if (self.UseAdvancedFilter)
+        {
+            if (self.IllegalWordsSearch == null)
+            {
+                self.IllegalWordsSearch = new IllegalWordsSearch();
+            }
+
+            self.IllegalWordsSearch.SetKeywords(keywords);
+        }
+        else
+        {
+            if (self.WordsSearch == null)
+            {
+                self.WordsSearch = new WordsSearch();
+            }
+
+            self.WordsSearch.SetKeywords(keywords);
+        }
+    }
+    
+    /// <summary>
+    /// 设置敏感词库和黑名单分级
+    /// </summary>
+    /// <param name="keywords">敏感词库</param>
+    /// <param name="blacklistTypes">黑名单分级</param>
+    public static void SetKeywords(this SensitiveWordComponent self, List<string> keywords, List<int> blacklistTypes)
+    {
+        if (self.UseAdvancedFilter)
+        {
+            if (self.IllegalWordsSearch == null)
+            {
+                self.IllegalWordsSearch = new IllegalWordsSearch();
+            }
+
+            self.IllegalWordsSearch.SetKeywords(keywords);
+            self.IllegalWordsSearch.SetBlacklist(blacklistTypes.ToArray());
+        }
+    }
+    
+    /// <summary>
+    /// 检查文本是否包含敏感词
+    /// </summary>
+    /// <param name="content">文本内容</param>
+    /// <returns></returns>
+    public static bool ContainsSensitiveWord(this SensitiveWordComponent self, string content)
+    {
+        if (string.IsNullOrEmpty(content))
+        {
+            return false;
+        }
+
+        if (self.UseAdvancedFilter)
+        {
+            return self.IllegalWordsSearch != null && self.IllegalWordsSearch.ContainsAny(content);
+        }
+        else
+        {
+            return self.WordsSearch != null && self.WordsSearch.ContainsAny(content);
+        }
+    }
+    
+    /// <summary>
+    /// 替换敏感词为指定字符
+    /// </summary>
+    /// <param name="content">文本内容</param>
+    /// <param name="replaceChar">替换的字符</param>
+    /// <returns></returns>
+    public static string ReplaceSensitiveWords(this SensitiveWordComponent self, string content, char replaceChar = '\0')
+    {
+        if (string.IsNullOrEmpty(content))
+        {
+            return content;
+        }
+
+        if (replaceChar == '\0')
+        {
+            replaceChar = self.ReplaceChar;
+        }
+
+        if (self.UseAdvancedFilter)
+        {
+            return self.IllegalWordsSearch == null ? content : self.IllegalWordsSearch.Replace(content, replaceChar);
+        }
+        else
+        {
+            return self.WordsSearch == null ? content : self.WordsSearch.Replace(content, replaceChar);
+        }
+    }
+    
+    /// <summary>
+    /// 设置跳词字符
+    /// </summary>
+    /// <param name="skipWords">跳词字符串</param>
+    /// <returns></returns>
+    public static void SetSkipWords(this SensitiveWordComponent self, string skipWords)
+    {
+        if (string.IsNullOrEmpty(skipWords))
+        {
+            return;
+        }
+        if (self.UseAdvancedFilter)
+        {
+            if (self.IllegalWordsSearch == null)
+            {
+                self.IllegalWordsSearch = new IllegalWordsSearch();
+            }
+
+            self.IllegalWordsSearch.SetSkipWords(skipWords);
+        }
+    }
+    
+    /// <summary>
+    /// 黑名单分级查询第一个
+    /// </summary>
+    /// <param name="content">文本内容</param>
+    /// <returns></returns>
+    public static IllegalWordsSearchResult FindFirst(this SensitiveWordComponent self, string content)
+        => self.UseAdvancedFilter && self.IllegalWordsSearch != null ? self.IllegalWordsSearch.FindFirst(content) : null;
+    
+    /// <summary>
+    /// 黑名单分级查询所有
+    /// </summary>
+    /// <param name="content">文本内容</param>
+    /// <returns></returns>
+    public static List<IllegalWordsSearchResult> FindAll(this SensitiveWordComponent self, string content)
+        => self.UseAdvancedFilter && self.IllegalWordsSearch != null ? self.IllegalWordsSearch.FindAll(content) : null;
+    
+    /// <summary>
+    /// 获取文本中所有的敏感词
+    /// </summary>
+    /// <param name="content">文本内容</param>
+    /// <returns></returns>
+    public static List<string> FindAllSensitiveWords(this SensitiveWordComponent self, string content)
+    {
+        List<string> result = new List<string>();
+
+        if (self.UseAdvancedFilter)
+        {
+            if (self.IllegalWordsSearch == null)
+            {
+                self.IllegalWordsSearch = new IllegalWordsSearch();
+            }
+
+            var matches = self.IllegalWordsSearch.FindAll(content);
+
+            foreach (var match in matches)
+            {
+                result.Add(match.Keyword);
+            }
+        }
+        else
+        {
+            if (self.WordsSearch == null)
+            {
+                self.WordsSearch = new WordsSearch();
+            }
+
+            var matches = self.WordsSearch.FindAll(content);
+
+            foreach (var match in matches)
+            {
+                result.Add(match.Keyword);
+            }
+        }
+
+        return result;
+    }
+    
+    /// <summary>
+    /// 获取文本中所有的敏感词
+    /// </summary>
+    /// <param name="content">文本内容</param>
+    /// <param name="result">敏感词结果</param>
+    /// <returns></returns>
+    public static void FindAllSensitiveWords(this SensitiveWordComponent self, string content, ref List<string> result)
+    {
+        if (result == null)
+        {
+            result = new List<string>();
+        }
+
+        if (self.UseAdvancedFilter)
+        {
+            if (self.IllegalWordsSearch == null)
+            {
+                self.IllegalWordsSearch = new IllegalWordsSearch();
+            }
+
+            var matches = self.IllegalWordsSearch.FindAll(content);
+
+            foreach (var match in matches)
+            {
+                result.Add(match.Keyword);
+            }
+        }
+        else
+        {
+            if (self.WordsSearch == null)
+            {
+                self.WordsSearch = new WordsSearch();
+            }
+
+            var matches = self.WordsSearch.FindAll(content);
+
+            foreach (var match in matches)
+            {
+                result.Add(match.Keyword);
+            }
+        }
+    }
+}
