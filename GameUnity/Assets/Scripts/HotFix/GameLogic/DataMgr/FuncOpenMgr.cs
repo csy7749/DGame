ddd2a2f161d02ba74170f7de98f9570eeea9c583
Amani <799375868@qq.com>
2026-03-15 22:07:50 +0800

    FuncOpenMgr

---
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/DataMgr/FuncOpenMgr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Fantasy;
+using GameProto;
+
+namespace GameLogic
+{
+    public class FuncOpenMgr : DataCenterModule<FuncOpenMgr>
+    {
+        private bool m_init;
+        private List<int> m_openFuncList = new List<int>();
+
+        #region Override
+
+        public override void OnRoleLogout()
+        {
+            m_init = false;
+            m_openFuncList.Clear();
+        }
+
+        #endregion
+
+        public bool CheckFuncOpen(FuncType funcType, bool showTips = false)
+            => CheckFuncOpen((int)funcType, showTips);
+
+        public bool CheckFuncOpen(int funcType, bool showTips = false)
+        {
+            bool isOpen = m_openFuncList.Contains(funcType);
+
+            if (!isOpen && showTips && TbFuncOpenConfig.TryGetValue(funcType, out var cfg))
+            {
+                UIModule.Instance.ShowTipsUI((uint)cfg.NoOpenTipsId);
+            }
+            return isOpen;
+        }
+    }
+}
---
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/IEvent/IFuncOpenLogic.cs
@@ -0,0 +1,20 @@
+﻿using DGame;
+using Fantasy;
+
+namespace GameLogic
+{
+    [EventInterface(EEventGroup.GroupLogic)]
+    public interface IFuncOpenLogic
+    {
+        /// <summary>
+        /// 系统功能开放数据变更
+        /// </summary>
+        void OnFuncOpenDataChange();
+        
+        /// <summary>
+        /// 新功能开启
+        /// </summary>
+        /// <param name="funcType">新功能类型</param>
+        void OnFuncOpen(FuncType funcType);
+    }
+}
