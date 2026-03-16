af4e7b613ecb64cc1fddf6fd5fb070d313c274e2
Jun <799375868@qq.com>
2026-03-16 17:07:28 +0800

    增加xml注释

--- GameUnity/Assets/Scripts/HotFix/GameLogic/Module/RedDotModule/RedDotItem.cs
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/Module/RedDotModule/RedDotItem.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 
 namespace GameLogic
 {
+	/// <summary>
+	/// 红点UI控件
+	/// </summary>
 	public class RedDotItem : UIWidget
 	{
 		#region 脚本工具生成的代码
@@ -25,6 +28,9 @@ namespace GameLogic
 		#region 字段
 
 		private RedDotNode m_redDotNode;
+		/// <summary>
+		/// 关联的红点节点
+		/// </summary>
 		public RedDotNode RedDotNode => m_redDotNode;
 		private bool m_isInit = false;
 
@@ -54,6 +60,10 @@ namespace GameLogic
 
 		#region 函数
 
+		/// <summary>
+		/// 初始化红点控件
+		/// </summary>
+		/// <param name="redDotNodeID">红点节点ID</param>
 		public void Init(int redDotNodeID)
 		{
 			if (m_redDotNode != null)
@@ -87,22 +97,38 @@ namespace GameLogic
 			OnRedDotValueChanged(m_redDotNode);
 		}
 
+		/// <summary>
+		/// 设置普通红点图片的显示状态
+		/// </summary>
+		/// <param name="isActive">是否显示</param>
 		public void SetImgRedActive(bool isActive)
 		{
 			m_imgRed.gameObject.SetActive(isActive);
 		}
 
+		/// <summary>
+		/// 设置数字红点图片的显示状态
+		/// </summary>
+		/// <param name="isActive">是否显示</param>
 		public void SetImgTextRedActive(bool isActive)
 		{
 			m_imgTextRed.gameObject.SetActive(isActive);
 		}
 
+		/// <summary>
+		/// 设置数字红点显示的数量
+		/// </summary>
+		/// <param name="count">红点数量</param>
 		public void SetImgTextRedCount(int count)
 		{
 			var countStr = count > 99 ? "99+" : count.ToString();
 			m_textRed.text = countStr;
 		}
 
+		/// <summary>
+		/// 设置NEW标签红点的显示状态
+		/// </summary>
+		/// <param name="isActive">是否显示</param>
 		public void SetImgNewRedActive(bool isActive)
 		{
 			m_goNewRed.SetActive(isActive);
