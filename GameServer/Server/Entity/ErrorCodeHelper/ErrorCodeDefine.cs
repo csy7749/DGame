5b5ae061599a16839a60086d56915adc73f149ac
Amani <799375868@qq.com>
2026-03-19 22:11:32 +0800

    Update ErrorCodeDefine.cs

--- GameServer/Server/Entity/ErrorCodeHelper/ErrorCodeDefine.cs
+++ GameServer/Server/Entity/ErrorCodeHelper/ErrorCodeDefine.cs
@@ -17,14 +17,14 @@ public static class ErrorCodeDefine
     public const uint LOGIN_REGISTER_PASSWORD_LESS_LIMIT = 1012; // 密码长度至少需8位
     public const uint REGISTER_TOW_PASSWORD_INCONSISTENT = 1013; // 两次输入的密码不一致
     public const uint REGISTER_SUCCESS = 1014; // 注册成功
-    
+
     public const uint CREATE_ROLE_SUCCESS = 2001; // 创建角色成功
     public const uint ROLE_CONFIG_NOT_FOUND = 2002; // 创角色配置不存在
     public const uint ROLE_NAME_DUPLICATE = 2003; // 角色名已存在
-    public const uint    ROLE_NAME_INVALID  = 2004; // 角色名无效（为空或包含非法字符）
-    public const uint ROLE_SEX_INVALID   = 2005; // 性别参数无效
-    public const uint     ROLE_NOT_FOUND    = 2006; // 角色不存在
-    public const uint ROLE_NOT_BELONG_TO_ACCOUNT  = 2007; // 角色不属于该账号
-    public const uint     ROLE_NAME_LENGTH_OUT_RANGE  = 2008; // 角色名长度不符，请使用中文2-6字，英文4-12字符
-    public const uint ROLE_NAME_CHAR_INVALID   = 2009; // 角色名字含有非法字符
+    public const uint ROLE_NAME_INVALID = 2004; // 角色名无效（为空或包含非法字符）
+    public const uint ROLE_SEX_INVALID = 2005; // 性别参数无效
+    public const uint ROLE_NOT_FOUND = 2006; // 角色不存在
+    public const uint ROLE_NOT_BELONG_TO_ACCOUNT = 2007; // 角色不属于该账号
+    public const uint ROLE_NAME_LENGTH_OUT_RANGE = 2008; // 角色名长度不符，请使用中文2-6字，英文4-12字符
+    public const uint ROLE_NAME_CHAR_INVALID = 2009; // 角色名字含有非法字符
 }
