2216ae5fd07f947575121f8c16780ff0211f4e47
Amani <799375868@qq.com>
2026-04-03 21:35:15 +0800

    LoadingUI

---
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/UI/Common/LoadingUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DGame;
+
+namespace GameLogic
+{
+	public partial class LoadingUI
+	{
+		#region 事件
+
+		private partial void OnSliderProgressChange(float value)
+		{
+		}
+
+		#endregion
+	}
+}
